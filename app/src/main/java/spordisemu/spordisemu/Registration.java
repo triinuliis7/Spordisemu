fa1f2aa938144b05baaaa8725a5acb5edb687649
Triinu Liis Kelder <triinuliis7@gmail.com>
2015-10-09 16:12:45 +0300

    Removed date of birth from registration, added landscape xml file for registration. Closes #61

--- app/src/main/java/spordisemu/spordisemu/Registration.java
+++ app/src/main/java/spordisemu/spordisemu/Registration.java
@@ -1,7 +1,5 @@
 package spordisemu.spordisemu;
 
-import android.app.DatePickerDialog;
-import android.app.Dialog;
 import android.content.res.Configuration;
 import android.graphics.Point;
 import android.os.Bundle;
@@ -9,7 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -22,11 +19,6 @@ import java.util.Calendar;
 
 public class Registration extends AppCompatActivity{
 
-    private DatePicker datePicker;
-    private Calendar calendar;
-    private TextView dateView;
-    private int year, month, day;
-
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
@@ -36,6 +28,7 @@ public class Registration extends AppCompatActivity{
         Point size = new Point();
         display.getSize(size);
         int height = size.y;
+        int width = size.x;
 
         TextView title = (TextView) findViewById(R.id.signUpText);
         ViewGroup.MarginLayoutParams titleParams = (ViewGroup.MarginLayoutParams) title.getLayoutParams();
@@ -58,89 +51,22 @@ public class Registration extends AppCompatActivity{
         Button signUpBtn = (Button) findViewById(R.id.signUpBtn);
         ViewGroup.MarginLayoutParams signUpBtnParams = (ViewGroup.MarginLayoutParams) signUpBtn.getLayoutParams();
 
-        LinearLayout layout1 = (LinearLayout) findViewById(R.id.layout1);
-        ViewGroup.MarginLayoutParams layout1Params = (ViewGroup.MarginLayoutParams) layout1.getLayoutParams();
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            LinearLayout layout1 = (LinearLayout) findViewById(R.id.linearLayout1);
+            ViewGroup.MarginLayoutParams layout1Params = (ViewGroup.MarginLayoutParams) layout1.getLayoutParams();
 
-        LinearLayout layout2 = (LinearLayout) findViewById(R.id.layout2);
-        ViewGroup.MarginLayoutParams layout2Params = (ViewGroup.MarginLayoutParams) layout2.getLayoutParams();
+            layout1Params.width = width / 2;
 
-        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
-            title.setTextSize(24);
-            titleParams.topMargin = 10;
-            signUpTextParams.topMargin = 5;
-            signUpTextParams.height = 70;
-            signUpBtnParams.topMargin = 10;
-            signUpBtnParams.height = 70;
-            layout1Params.height = 70;
-            layout2Params.height = 70;
-            signUpMailParams.height = 70;
-            signUpPwEdit1Params.height = 70;
-            signUpPwEdit2Params.height = 70;
+            LinearLayout layout2 = (LinearLayout) findViewById(R.id.linearLayout2);
+            ViewGroup.MarginLayoutParams layout2Params = (ViewGroup.MarginLayoutParams) layout2.getLayoutParams();
+
+            layout2Params.width = width / 2;
         }
-        else {
-            titleParams.topMargin = height / 6;
-        }
+
+        titleParams.topMargin = height / 6;
 
         ViewGroup.MarginLayoutParams radioGroupParams = (ViewGroup.MarginLayoutParams) radioGroup.getLayoutParams();
 
-        dateView = (TextView) findViewById(R.id.textView4);
-        calendar = Calendar.getInstance();
-        year = calendar.get(Calendar.YEAR);
-
-        month = calendar.get(Calendar.MONTH);
-        day = calendar.get(Calendar.DAY_OF_MONTH);
-
-        String dayS = Integer.toString(day);
-        String monthS = Integer.toString(month + 1);
-        String yearS = Integer.toString(year);
-
-        if (day < 10) {
-            dayS = "0" + Integer.toString(day);
-        }
-        if (month < 9) {
-            monthS = "0" + Integer.toString(month);
-        }
-        showDate(yearS, monthS, dayS);
-
-        /*LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) radioGroup.getLayoutParams();
-        params.width = 250 - suguText.getWidth();
-        radioGroup.setLayoutParams(params);*/
-    }
-
-    public void setDate(View view) {
-        showDialog(999);
-        //Toast.makeText(getApplicationContext(), "ca", Toast.LENGTH_SHORT)
-        //        .show();
-    }
-
-    @Override
-    protected Dialog onCreateDialog(int id) {
-        if (id == 999) {
-            return new DatePickerDialog(this, myDateListener, year, month, day);
-        }
-        return null;
-    }
-
-    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
-        @Override
-        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
-            String arg1S = Integer.toString(arg1); //year
-            String arg2S = Integer.toString(arg2 + 1); //month
-            String arg3S = Integer.toString(arg3); //day
-
-            if (arg3 < 10) {
-                arg3S = "0" + Integer.toString(arg3);
-            }
-            if (arg2 < 9) {
-                arg2S = "0" + Integer.toString(arg2 + 1);
-            }
-            showDate(arg1S, arg2S, arg3S);
-        }
-    };
-
-    private void showDate(String year, String month, String day) {
-        dateView.setText(new StringBuilder().append(day).append(".")
-                .append(month).append(".").append(year));
     }
 
     @Override
