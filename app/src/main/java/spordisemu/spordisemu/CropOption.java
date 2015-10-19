23e4302f778511b8f859b6d9fc2c3fd989b1025c
Kelian Kaio <kelian14@gmail.com>
2015-10-19 09:52:06 +0300

    Forgot to add CropOption file #79

---
+++ app/src/main/java/spordisemu/spordisemu/CropOption.java
@@ -0,0 +1,13 @@
+package spordisemu.spordisemu;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Kelian on 18/10/2015.
+ */
+public class CropOption {
+    public CharSequence title;
+    public Drawable icon;
+    public Intent appIntent;
+}
