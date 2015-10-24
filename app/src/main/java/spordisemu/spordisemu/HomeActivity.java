e4baeb2ef4b81120b74e041a133f5351afcdd718
ingrid94 <ingrid.kaasik@eesti.ee>
2015-10-24 16:11:57 +0300

    User can see practices after logging in or registrating. #27

--- app/src/main/java/spordisemu/spordisemu/HomeActivity.java
+++ app/src/main/java/spordisemu/spordisemu/HomeActivity.java
@@ -6,11 +6,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.security.Timestamp;
+import java.util.Date;
 
 /**
  * Created by Triinu Liis on 11/10/2015.
  */
 public class HomeActivity extends AppCompatActivity{
+
+    public final static String apiURL = "http://private-6358e-spordisemu1.apiary-mock.com";
+
+
+    String[] sportsArray = {"Jalgpall", "Korvpall", "Sulgpall", "Korvpall"};
+
 
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -19,6 +30,13 @@ public class HomeActivity extends AppCompatActivity{
         setTitle("Pealeht");
 
         setContentView(R.layout.activity_home);
+
+
+        ListView sportsListView = (ListView) findViewById(R.id.sports_list);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String> (this,
+                android.R.layout.simple_list_item_1, sportsArray);
+        sportsListView.setAdapter(arrayAdapter);
+
     }
 
     @Override
