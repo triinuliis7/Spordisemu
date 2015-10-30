3ca5bfe20cdbb34107990d0745dd9255fbb17e55
Triinu Liis Kelder <triinuliis7@gmail.com>
2015-10-30 11:38:05 +0200

    Added Google Maps to PracticeView resolves #32

--- app/src/main/java/spordisemu/spordisemu/PracticeViewActivity.java
+++ app/src/main/java/spordisemu/spordisemu/PracticeViewActivity.java
@@ -2,16 +2,34 @@ package spordisemu.spordisemu;
 
 import android.app.ActionBar;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Point;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -19,22 +37,25 @@ import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by ingrid on 10/25/15.
  */
 public class PracticeViewActivity extends AppCompatActivity {
 
+    private GoogleMap mMap;
+    private boolean chooseMarker = false;
+    private Address address;
+
     protected void onCreate(Bundle savedInstanceState) {
 
-        requestWindowFeature(Window.FEATURE_ACTION_BAR);
         super.onCreate(savedInstanceState);
-
-        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
 
         setContentView(R.layout.activity_practice_view);
 
@@ -47,12 +68,71 @@ public class PracticeViewActivity extends AppCompatActivity {
         TextView user = (TextView) findViewById(R.id.user);
         user.setText(getResources().getString(R.string.korraldaja) + " " + getIntent().getStringExtra("user"));
 
+        setUpMapIfNeeded();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+
+            SupportMapFragment mMapFragment = (SupportMapFragment) (getSupportFragmentManager()
+                    .findFragmentById(R.id.detailedMap));
+
+            mMap = mMapFragment.getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+
+        TextView location = (TextView) findViewById(R.id.location);
+        String locationString = location.getText().toString();
+        LatLng latLng = getLocationFromAddress(this, locationString);
+        mMap.addMarker(new MarkerOptions().position(latLng).title(locationString)).showInfoWindow();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 6));
+
+    }
+
+    public LatLng getLocationFromAddress(Context context,String strAddress) {
+
+        Geocoder coder = new Geocoder(context);
+        List<Address> address;
+        LatLng p1 = null;
+
+        try {
+            address = coder.getFromLocationName(strAddress, 5);
+            if (address == null) {
+                return null;
+            }
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            p1 = new LatLng(location.getLatitude(), location.getLongitude() );
+
+        } catch (Exception ex) {
+
+            ex.printStackTrace();
+        }
+
+        return p1;
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_result, menu);
         return true;
     }
 
@@ -61,42 +141,14 @@ public class PracticeViewActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-
         int id = item.getItemId();
 
-        switch(item.getItemId()) {
-            case R.id.pealeht:
-                Intent homeIntent = new Intent(getApplicationContext(), HomeActivity.class);
-                startActivity(homeIntent);
-                break;
-            case R.id.profiil:
-                Intent profileIntent = new Intent(getApplicationContext(), ProfileActivity.class);
-                startActivity(profileIntent);
-                break;
-            case R.id.sobrad:
-                Intent friendsIntent = new Intent(getApplicationContext(), FriendsActivity.class);
-                startActivity(friendsIntent);
-                break;
-            case R.id.action_settings:
-                Intent settingsIntent = new Intent(getApplicationContext(), SettingsActivity.class);
-                startActivity(settingsIntent);
-                break;
-            case R.id.logivalja:
-                Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(mainIntent);
-                break;
-            case android.R.id.home:
-                this.finish();
-                return true;
-            default:
-                Toast.makeText(getApplicationContext(),
-                        "Midagi läks valesti :(",
-                        Toast.LENGTH_SHORT).show();
-                break;
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
 
-        //Return false to allow normal menu processing to proceed,
-        //true to consume it here.
-        return false;
+        return super.onOptionsItemSelected(item);
     }
+
 }
