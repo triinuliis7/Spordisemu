b8737e26152ebed38d6537bd2fccccc207c536ec
Triinu Liis Kelder <triinuliis7@gmail.com>
2015-11-01 20:15:20 +0200

    Espresso tests, test for RegistrationActivity #63

---
+++ app/src/androidTest/java/spordisemu/spordisemu/RegistrationActivityTest.java
@@ -0,0 +1,67 @@
+package spordisemu.spordisemu;
+
+import android.support.test.InstrumentationRegistry;
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.junit.Before;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.isDialog;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by Triinu Liis on 01/11/2015.
+ */
+public class RegistrationActivityTest  extends ActivityInstrumentationTestCase2<RegistrationActivity> {
+
+    private RegistrationActivity registrationActivity;
+
+    public RegistrationActivityTest() {
+        super(RegistrationActivity.class);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        registrationActivity = getActivity();
+    }
+
+    public void testChangeTextEverythingOK() {
+        // Type text and then press the button.
+        onView(withId(R.id.SignUpUserEdit))
+                .perform(typeText("Test"), closeSoftKeyboard());
+        onView(withId(R.id.SignUpUserEdit))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.signUpNameEdit))
+                .perform(typeText("Test"), closeSoftKeyboard());
+        onView(withId(R.id.signUpNameEdit))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.SignUpLastEdit))
+                .perform(typeText("Test"), closeSoftKeyboard());
+        onView(withId(R.id.SignUpLastEdit))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.signUpMailEdit))
+                .perform(typeText("test@mail.ee"), closeSoftKeyboard());
+        onView(withId(R.id.signUpMailEdit))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.signUpPwEdit1))
+                .perform(typeText("testtest"), closeSoftKeyboard());
+        onView(withId(R.id.signUpPwEdit1))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.signUpPwEdit2))
+                .perform(typeText("testtest"), closeSoftKeyboard());
+        onView(withId(R.id.signUpPwEdit2))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.signUpBtn)).perform(click());
+
+        // Check if new activity is open
+        onView(withId(R.id.textView3)).check(matches(isDisplayed()));
+    }
+}
