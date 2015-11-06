06b92d14e89e14a93087ab2ac9ba74f170abaa99
Kelian Kaio <kelian14@gmail.com>
2015-11-06 18:10:49 +0200

    Added post request for creating a practice + after creating a practice it takes to practice view, -> closes #28, added ResultActivity again for debugging purposes.

--- app/src/main/java/spordisemu/spordisemu/CreatePracticeActivity.java
+++ app/src/main/java/spordisemu/spordisemu/CreatePracticeActivity.java
@@ -1,9 +1,14 @@
 package spordisemu.spordisemu;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.ArrayAdapter;
@@ -15,10 +20,17 @@ import android.widget.Spinner;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+
+import javax.xml.transform.Result;
 
 /**
  * Created by Kelian on 31/10/2015.
@@ -26,6 +38,7 @@ import java.util.Date;
 public class CreatePracticeActivity extends AppCompatActivity {
 
     private AlphaAnimation buttonClick = new AlphaAnimation(1.0F, 0.5F);
+    public final static String EXTRA_MESSAGE = "";
 
     static Spinner sportSpinner;
     static Spinner raskustaseSpinner;
@@ -45,6 +58,11 @@ public class CreatePracticeActivity extends AppCompatActivity {
     static EditText max;
 
     static RadioGroup gender;
+
+    public String j_date = "";
+    public String j_location = "";
+    public String j_title = "";
+    public String j_practice_id = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,14 +106,20 @@ public class CreatePracticeActivity extends AppCompatActivity {
     public void createPractice(View view) {
         view.startAnimation(buttonClick);
         if (checkIfDataCorrect()){
+            String timestamp = generateTimestamp();
             JSONObject json = new JSONObject();
             try {
                 json.put("sports", sportSpinner.getSelectedItem().toString());
                 json.put("level", raskustaseSpinner.getSelectedItem().toString());
-                // poolik
+                json.put("gender", getGender());
+                json.put("timestamp", timestamp);
+                json.put("location", location.getText().toString());
+                json.put("min", getMinMax(min));
+                json.put("max", getMinMax(max));
             } catch (JSONException e) {
                 e.printStackTrace();
             }
+            postJson(json);
         }
 
     }
@@ -105,7 +129,8 @@ public class CreatePracticeActivity extends AppCompatActivity {
         if (checkIfSportsEmpty()) {
             done = false;
         }
-        if (checkFieldNotCorrect(hour, 12)) {
+
+        if (checkFieldNotCorrect(hour, 24))  {
             done = false;
         }
         if (checkFieldNotCorrect(minutes, 60)) {
@@ -131,7 +156,7 @@ public class CreatePracticeActivity extends AppCompatActivity {
     }
 
     public boolean checkIfSportsEmpty() {
-        if(sportSpinner.getSelectedItem().toString().equals( "Spordiala" ) ||
+        if(sportSpinner.getSelectedItem().toString().equals("Spordiala") ||
                 raskustaseSpinner.getSelectedItem().toString().equals( "Tase")){
             AlertDialog.Builder alert = new AlertDialog.Builder(CreatePracticeActivity.this);
             alert.setMessage(getResources().getString(R.string.valimataSport));
@@ -145,10 +170,10 @@ public class CreatePracticeActivity extends AppCompatActivity {
             AlertDialog alert11 = alert.create();
             alert11.show();
 
-            return false;
+            return true;
         }
 
-        return true;
+        return false;
     }
 
     public boolean checkIfEmpty(EditText name) {
@@ -209,5 +234,199 @@ public class CreatePracticeActivity extends AppCompatActivity {
         return done;
     }
 
+    public String generateTimestamp() {
+        String timestamp = year.getText().toString() + "-" + month.getText().toString() + "-" +
+                day.getText().toString() + " " + hour.getText().toString() + ":" +
+                minutes.getText().toString() + ":00";
+        return timestamp;
+    }
+
+    public int getGender() {
+        int RadioButtonID = gender.getCheckedRadioButtonId();
+        View radioButton = gender.findViewById(RadioButtonID);
+        return gender.indexOfChild(radioButton);
+    }
+
+    public int getMinMax(EditText name) {
+        int result = 0;
+        if(name.getText().length() != 0){
+            result = Integer.parseInt(name.getText().toString());
+        }
+        return result;
+    }
+
+    public void postJson(JSONObject json) {
+        String apiURL = getResources().getString(R.string.apiUrl);
+        String urlString = apiURL + "/practices";
+        new CallAPI().execute(urlString, json.toString(),"createPractice", "post");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_result, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void initializePractice(JSONObject json) {
+        try {
+
+            String id = json.getString("user_id").toString();
+
+            String apiURL = getResources().getString(R.string.apiUrl);
+            String urlString = apiURL + "/users/" + id;
+            new CallAPI().execute(urlString, "", "user", "get");
+
+            j_date = json.get("timestamp").toString();
+            j_location = json.getString("location").toString();
+            j_title = json.getString("type").toString();
+            j_practice_id = json.getString("practice_id").toString();
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void initializeUser(JSONObject json) {
+
+        Intent PracticeViewIntent = new Intent(getApplicationContext(), PracticeViewActivity.class);
+        try {
+            PracticeViewIntent.putExtra("user", json.getString("firstname").toString() + " " + json.getString("lastname").toString());
+            PracticeViewIntent.putExtra("date", j_date);
+            PracticeViewIntent.putExtra("location", j_location);
+            PracticeViewIntent.putExtra("title", j_title);
+            PracticeViewIntent.putExtra("practice_id", j_practice_id);
+            PracticeViewIntent.putExtra("loggedIn_id", getIntent().getStringExtra("loggedIn_id"));
+            startActivity(PracticeViewIntent);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class CallAPI extends AsyncTask<String, String, String> {
+
+        ProgressDialog dialog = new ProgressDialog(CreatePracticeActivity.this);
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            dialog.setMessage(getResources().getString(R.string.wait));
+            dialog.setIndeterminate(true);
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String urlString = params[0];
+            String request = params[1];
+            String type = params[2];
+            String getpost = params[3];
+            String response;
+
+            StringBuffer jsonResponse = new StringBuffer();
+
+            if(getpost.equals("post")) {
+                // HTTP POST
+                try {
+                    String line;
+                    URL url = new URL(urlString);
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                    urlConnection.setDoInput(true);
+                    urlConnection.setDoOutput(true);
+                    urlConnection.setRequestMethod("POST");
+                    urlConnection.setRequestProperty("Accept", "application/json");
+                    urlConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                    OutputStreamWriter writer = new OutputStreamWriter(urlConnection.getOutputStream(), "UTF-8");
+                    writer.write(request);
+                    writer.close();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    while ((line = br.readLine()) != null) {
+                        jsonResponse.append(line);
+                    }
+                    br.close();
+                    urlConnection.disconnect();
+                } catch (Exception e ) {
+                    System.out.println(e.getMessage());
+                    return e.getMessage();
+                }
+            } else if (getpost.equals("get")) {
+                // HTTP GET
+                try {
+                    String line;
+                    URL url = new URL(urlString);
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                    urlConnection.setDoInput(true);
+                    urlConnection.setRequestMethod("GET");
+                    urlConnection.setRequestProperty("Accept", "application/json");
+                    urlConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                    BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    while ((line = br.readLine()) != null) {
+                        jsonResponse.append(line);
+                    }
+                    br.close();
+                    urlConnection.disconnect();
+                } catch (Exception e ) {
+                    System.out.println(e.getMessage());
+                    return e.getMessage();
+                }
+            }
+
+            response = jsonResponse.toString();
+            if(type.equals("createPractice")) {
+                response = "c" + response;
+            } else if (type.equals("practice")) {
+                response = "p" + response;
+            } else if (type.equals("user")) {
+                response = "u" + response;
+            }
+            return response;
+        }
+
+        protected void onPostExecute(String result) {
+            dialog.dismiss();
+            if(result.charAt(0) == 'c') {
+                try {
+                    JSONObject json = new JSONObject(result.substring(1));
+                    String apiURL = getResources().getString(R.string.apiUrl);
+                    String urlString = apiURL + "/practices/" + json.get("practice_id").toString();
+                    new CallAPI().execute(urlString, "practice", "practice", "get");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else if(result.charAt(0) == 'p') {
+                try {
+                    initializePractice(new JSONObject(result.substring(1)));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else if(result.charAt(0) == 'u') {
+                try {
+                    initializeUser(new JSONObject(result.substring(1)));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
 
